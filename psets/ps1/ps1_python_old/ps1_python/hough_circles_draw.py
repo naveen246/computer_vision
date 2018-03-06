f1c343f28503354968f26ea5818db8a3a872f616
Naveen <naveen246@gmail.com>
2018-03-06 16:29:09 +0530

    Hough Transform for Circles

--- psets/ps1/ps1_python_old/ps1_python/hough_circles_draw.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-def hough_circles_draw(img, outfile, peaks, radius):
-    for peak in peaks:
-        cv2.circle(img, tuple(peak[::-1]), radius, (0,255,0), 2)
-    cv2.imwrite(outfile, img)
-    return img
